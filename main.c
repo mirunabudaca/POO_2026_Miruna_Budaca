af7b91ba82e1dfa34fb5fb2df6c9b6b1f9118c02
mirunabudaca <mirunabudaca@gmail.com>
2026-02-25 16:53:22 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int conversie_int(char* s) {
+    int rez = 0;
+    int i = 0;
+    int semn = 1;
+
+    if (s[i] == '-') {
+        semn = -1;
+        i++;
+    }
+
+    while (s[i] >= '0' && s[i] <= '9') {
+        rez = rez * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return semn * rez;
+}
+
+int main() {
+
+    //ex1:
+
+    FILE* f = fopen("in.txt", "r");
+
+    if (f == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+    char buffer[100];
+    int sum = 0;
+
+    while (fgets(buffer, sizeof(buffer), f)) {
+        sum += conversie_int(buffer);
+    }
+
+    printf("Sum = %d\n", sum);
+
+    fclose(f);
+    return 0;
+}
